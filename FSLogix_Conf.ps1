8ae3d719cbb411fd3bb600a436360a7fe5fbdfca
Naor Yamin <65827018+NaorYamin@users.noreply.github.com>
2023-06-12 14:53:36 +0300

    Add files via upload

---
+++ FSLogix_Conf.ps1
@@ -0,0 +1,11 @@
+﻿#FSLogix Configuration
+write-host "Configuring FSLogix"
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ClearCacheOnLogOff" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "KeepLocalDir" -Value 0 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 80000 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "VHDX" -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "PreventLoginWithFailure" -Value 1 -force
+
+
