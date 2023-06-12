8f2a28d53d34d303e4d78d6b3a66fc8efad45144
Naor Yamin <65827018+NaorYamin@users.noreply.github.com>
2023-06-12 14:57:45 +0300

    Update FSLogix_Conf.ps1

--- FSLogix_Conf.ps1
+++ FSLogix_Conf.ps1
@@ -4,7 +4,6 @@ write-host "Configuring FSLogix"
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ClearCacheOnLogOff" -Value 1 -force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "KeepLocalDir" -Value 0 -force
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 80000 -force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "VHDX" -force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "PreventLoginWithFailure" -Value 1 -force
 
